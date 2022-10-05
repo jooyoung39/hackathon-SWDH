3bf91371271b1bb77d2fe87c396b8e9f09d7e812
Ichiya Luna <akkyu0724@gmail.com>
2022-10-05 21:39:26 +0900

    :sparkles: Main entrypoint updated

--- api/app.js
+++ api/app.js
@@ -1,25 +1,12 @@
+//express module
 const express = require("express");
 const app = express();
-const port = 3000;
 
-var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: "db.lunabi.co.kr",
-  user: "swdh-api",
-  password: "dustpgozjxhs",
-});
+//express-router
+const indexRouter = require("./routes/index");
+const accountRouter = require("./routes/account");
 
-app.get("/", (req, res) => {
-  connection.connect();
+app.use("/", indexRouter);
+app.use("/account", accountRouter);
 
-  connection.query("SELECT 1 + 1 AS solution", function (err, rows, fields) {
-    if (err) throw err;
-    res.send("The solution is: " + rows[0].solution);
-  });
-
-  connection.end();
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.listen(3000, () => console.log("YS-SWDH-Hackathon-2022!"));
