9cdcc0f72362d0603b1099b847bea8c0f90e632c
Ichiya Luna <akkyu0724@gmail.com>
2022-10-06 03:03:44 +0900

    fix

--- api/app.js
+++ api/app.js
@@ -5,6 +5,7 @@ const app = express();
 //express-router
 const indexRouter = require("./routes/index");
 const accountRouter = require("./routes/account");
+const symptomsRouter = require("./routes/symptoms");
 
 //cors
 const cors = require("cors");
@@ -16,5 +17,6 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/account", accountRouter);
+app.use("/symptoms", symptomsRouter);
 
 app.listen(3000, () => console.log("YS-SWDH-Hackathon-2022!"));
--- api/routes/symptoms.js
+++ api/routes/symptoms.js
@@ -31,3 +31,5 @@ router.get("/", (req, res) => {
     });
   });
 });
+
+module.exports = router;
