280d4e7f7b632362c93deb7cbc54f74c4154cbcf
Ichiya Luna <akkyu0724@gmail.com>
2022-10-06 19:46:03 +0900

    :sparkles: Add verification to login modal

--- web/src/components/LoginModal.js
+++ web/src/components/LoginModal.js
@@ -17,6 +17,7 @@ const LoginModal = (props) => {
 
   const [isOpen, setIsOpen] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+  const [isInvalid, setIsInvalid] = useState(false);
 
   const maxLengthCheck = (e) => {
     const limit = 10;
@@ -36,14 +37,17 @@ const LoginModal = (props) => {
         },
       })
       .then((res) => {
-        setIsLoading(false);
-        console.log(res.data.account);
-        const code = res.data.code;
-        if (code === 500) {
+        const code = res.status;
+
+        if (code === 204) {
+          setIsInvalid(true);
+        } else {
+          dispatch(loginUser(res.data.account));
+          setIsOpen(false);
+          props.onClose();
         }
-        dispatch(loginUser(res.data.account));
-        setIsOpen(false);
-        props.onClose();
+
+        setIsLoading(false);
       });
   };
 
@@ -58,33 +62,38 @@ const LoginModal = (props) => {
         />
       </Modal.Header>
       {props.show && (
-        <Modal.Body style={{ marginTop: "-1rem", paddingTop: "1rem" }}>
+        <Modal.Body style={{ marginTop: "-1rem", paddingTop: "1rem", textAlign: "left" }}>
           <Form onSubmit={Login}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>학번</Form.Label>
-              <Form.Control
-                type="number"
-                pattern="\d*"
-                maxLength="10"
-                placeholder="학번을 입력해주세요"
-                value={studentNum}
-                onInput={maxLengthCheck}
-                required
-              />
-            </Form.Group>
+            <div>
+              <Form.Group className="mb-3">
+                <Form.Label>학번</Form.Label>
+                <Form.Control
+                  type="number"
+                  pattern="\d*"
+                  maxLength="10"
+                  placeholder="학번을 입력해주세요"
+                  value={studentNum}
+                  onInput={maxLengthCheck}
+                  required
+                  isInvalid={isInvalid}
+                />
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>비밀번호</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="비밀번호를 입력해주세요"
-                value={password}
-                onInput={(e) => setPassword(e.target.value)}
-                required
-              />
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>비밀번호</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="비밀번호를 입력해주세요"
+                  value={password}
+                  onInput={(e) => setPassword(e.target.value)}
+                  required
+                  isInvalid={isInvalid}
+                />
+                <Form.Control.Feedback type="invalid">학번 또는 비밀번호를 확인해 주세요.</Form.Control.Feedback>
+              </Form.Group>
+            </div>
             <div className="d-grid">
-              <Button variant="primary" type="submit" disabled={isLoading}>
+              <Button type="submit" disabled={isLoading}>
                 로그인
               </Button>
             </div>
